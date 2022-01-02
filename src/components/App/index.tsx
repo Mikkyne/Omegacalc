f7e56c4395a7527afb033a2d8fb9415b176a405d
David Workman <me@workmad3.com>
2022-01-02 19:56:47 +0000

    Getting an initial layout set up

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Grid } from 'semantic-ui-react'
 
 const App = () => {
-  return <div>hello</div>
+  return (
+    <Grid>
+      <Grid.Row>
+        <Grid.Column width={16} textAlign='center' color='black' stretched>
+          <div>Save input</div>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={8} color='black'>
+          <div>Buttons</div>
+        </Grid.Column>
+        <Grid.Column width={8} color='black'>
+          <div>Info area</div>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )
 }
 
 export default App
