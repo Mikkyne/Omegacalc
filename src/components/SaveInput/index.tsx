1341cecb175406d057aae466f6e9fe9b7fb87728
David Workman <me@workmad3.com>
2022-01-05 13:34:59 +0000

    Restoring decode save button

--- src/components/SaveInput/index.tsx
+++ src/components/SaveInput/index.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import { Input } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Button, Input, Grid } from 'semantic-ui-react'
 
 interface SaveInputProps {
   setSave:Function
 }
 
 const SaveInput = ({setSave}:SaveInputProps) => {
+  const [saveInput, setSaveInput] = useState('')
   const saveChanged = (newSave:string) => {
     const decodedSave = atob(newSave)
     setSave(JSON.parse(decodedSave))
   }
 
-  return <Input onChange={(e) => saveChanged(e.target.value)}
-                fluid
-                placeholder='Enter Save here..'
-                size='massive'
-                />
+  return (
+    <Grid>
+      <Grid.Row>
+        <Grid.Column width={4}>
+          <Input fluid placeholder='Enter save here...' size='massive' onChange={(e) => setSaveInput(e.target.value)} value={saveInput} />
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <Button onClick={() => saveChanged(saveInput)}>Decode Save</Button>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )
 }
 
 export default SaveInput
