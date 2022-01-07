f43b3b5de64fdec226a98daafd3690209531f730
David Workman <me@workmad3.com>
2022-01-07 23:33:09 +0000

    Remove interval, no longer needed

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect } from 'react'
 
 interface TimerProps {
-  interval: number,
   setTimer: Function,
   currentTimer: Date,
 }
 
-const Timer = ({ interval, setTimer, currentTimer }: TimerProps) => {
+const Timer = ({ setTimer, currentTimer }: TimerProps) => {
   useEffect(() => {
     const timeoutId = requestAnimationFrame(() => setTimer(new Date()))
     return () => cancelAnimationFrame(timeoutId)
-  }, [interval, currentTimer])
+  }, [currentTimer])
 
   return <></>
 }
