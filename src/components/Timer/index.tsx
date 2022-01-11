61035ae467ef89270cade1eb797695a16e8765a6
Mikkyne <96960638+Mikkyne@users.noreply.github.com>
2022-01-11 22:50:11 +0100

    Update index.tsx

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react'
-import TimerContext from '../../contexts/timerContext'
+import React, { useEffect } from 'react'
 
 interface TimerProps {
 	interval: number,
